30554446e19eb5a39c174e38419db70f3988369c
vitoraraujodev <vitoraraujo.dev@gmail.com>
2019-11-17 20:25:03 -0300

    Scroll Infinito

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -37,19 +37,39 @@ export default class User extends Component {
 
   state = {
     stars: [],
-    loading: false,
+    loading: true,
+    page: 1,
   };
 
   async componentDidMount() {
+    this.load();
+  }
+
+  load = async (page = 1) => {
+    const { stars } = this.state;
     const { navigation } = this.props;
     const user = navigation.state.params;
 
-    this.setState({ loading: true });
+    const response = await api.get(`/users/${user.login}/starred`, {
+      params: { page },
+    });
 
-    const response = await api.get(`/users/${user.login}/starred`);
+    this.setState({
+      stars: page >= 2 ? [...stars, ...response.data] : response.data,
+      page,
+      loading: false,
+      refreshing: false,
+    });
+  };
 
-    this.setState({ stars: response.data, loading: false });
-  }
+  loadMore = () => {
+    const { page } = this.state;
+
+    const nextPage = page + 1;
+
+    this.load(nextPage);
+  };
+
 
   render() {
     const { navigation } = this.props;
@@ -70,6 +90,8 @@ export default class User extends Component {
           <Stars
             data={stars}
             keyExtractor={star => String(star.id)}
+            onEndReachedThreshold={0.2}
+            onEndReached={this.loadMore}
             renderItem={({ item }) => (
               <Starred>
                 <OwnerAvatar source={{ uri: item.owner.avatar_url }} />
